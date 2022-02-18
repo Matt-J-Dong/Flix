e1aa90ec35ddb98fdeb2ec8212a044d5595db286
Matthew Dong <mjd9571@nyu.edu>
2022-02-18 17:26:14 -0500

    Finished Flix Project

---
+++ FlixCodingProject/MovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCell.swift
+//  FlixCodingProject
+//
+//  Created by Matthew Dong on 2/17/22.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ FlixCodingProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlixCodingProject
+//
+//  Created by Matthew Dong on 2/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
