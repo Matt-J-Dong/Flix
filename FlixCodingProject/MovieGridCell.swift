974816ddb259475af15a47d857cc2a95c650e70c
Matthew Dong <95544597+Matt-J-Dong@users.noreply.github.com>
2022-02-24 18:38:04 -0500

    Flix Part 2 created

---
+++ FlixCodingProject/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  FlixCodingProject
+//
+//  Created by Matthew Dong on 2/24/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
